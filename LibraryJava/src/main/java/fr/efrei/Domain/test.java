04c9cf9a57928016e9197acc05cda413750e67ba
Candice Mamou <candice.mamou@efrei.net>
2024-12-11 13:55:09 +0200

    Domain 1st step

--- LibraryJava/src/main/java/fr/efrei/Domain/test.java
+++
@@ -1,8 +0,0 @@
-package main.java.fr.efrei.Domain;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-
-}
---
+++ LibraryJava/src/main/java/fr/efrei/domain/Book.java
@@ -0,0 +1,109 @@
+package fr.efrei.domain;
+
+public class Book {
+
+    //attributes
+    public int id;
+    public String title;
+    public String author;
+    public String isbn;
+    public String category;
+    public int publicationYear;
+    public int totalCopies;
+    public int availableCopies;
+    public int loanCount;
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public int getTotalCopies() {
+        return totalCopies;
+    }
+
+    public void setTotalCopies(int totalCopies) {
+        this.totalCopies = totalCopies;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    public int getLoanCount() {
+        return loanCount;
+    }
+
+    public void setLoanCount(int loanCount) {
+        this.loanCount = loanCount;
+    }
+    public void incrementLoanCount() {
+        this.loanCount++;
+    }
+
+    //to String
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", category='" + category + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", totalCopies=" + totalCopies +
+                ", availableCopies=" + availableCopies +
+                ", loanCount=" + loanCount +
+                '}';
+    }
+}
