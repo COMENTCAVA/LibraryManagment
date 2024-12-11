315ed126d69246e7f2544f48a91d90adb4a1abe8
Candice Mamou <candice.mamou@efrei.net>
2024-12-11 14:16:51 +0200

    delete test class

--- LibraryJava/src/main/java/fr/efrei/factory/Test3.java
+++
@@ -1,9 +0,0 @@
-package main.java.fr.efrei.Factory;
-
-public class Test3 {
-
-    public static void main(String[] args) {
-        System.out.println("coucou");
-    }
-
-}
--- LibraryJava/src/main/java/fr/efrei/factory/test2.java
+++
@@ -1,7 +0,0 @@
-package main.java.fr.efrei.Factory;
-
-public class test2 {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-}
